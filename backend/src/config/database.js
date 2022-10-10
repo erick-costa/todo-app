b1b7257c91a24593a6e592a2f8f9f7414165b4b6
Erick Costa <erickcosta2509@gmail.com>
2022-10-10 14:51:06 -0300

    ODM e criação da API REST

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect("mongodb://localhost:27017/todo", {
+module.exports = mongoose.connect("mongodb://127.0.0.1/todo", {
   useMongoClient: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
