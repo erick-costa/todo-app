1ac30f4ae94ae76a2394b77397dd752360724fa4
Erick Costa <erickcosta2509@gmail.com>
2022-10-20 21:47:15 -0300

    frontend - componente menu

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Todo from "../todo/todo"
 import About from "../about/about"
+import Menu from "../template/menu"
 
 require("bootstrap/dist/css/bootstrap.min.css")
 require("font-awesome/css/font-awesome.min.css")
 
 const App = (props) => (
   <div className="container">
+    <Menu />
     <Todo />
     <About />
   </div>
