7fec4e12f977920a08fc9d58791f1695419fffb9
Erick Costa <erickcosta2509@gmail.com>
2022-10-20 22:10:40 -0300

    frontend - component PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,8 +1,45 @@
 import React from "react"
+import PageHeader from "../template/pageHeader"
 
 const About = (props) => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="Sobre" small="Nós" />
+
+    <h2>Nossa história</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in laoreet
+      leo. Nunc bibendum elementum rhoncus. Mauris vitae magna at diam
+      ullamcorper convallis sed vitae sapien. Cras vel commodo nisi. Quisque
+      fringilla, justo non interdum gravida, massa libero pulvinar sem, sit amet
+      gravida nulla dui et nibh. Aenean viverra enim nisl, vitae posuere ipsum
+      mollis nec. Nullam ut tortor ex. Donec aliquam pellentesque lorem et
+      facilisis. Curabitur malesuada arcu a enim scelerisque, ac blandit enim
+      porttitor. Morbi bibendum vitae velit eget ornare. Mauris sit amet
+      placerat libero. Morbi a lobortis libero. Suspendisse lacinia at orci vel
+      dignissim.
+    </p>
+    <h2>Missão e visão</h2>
+    <p>
+      Cras volutpat vel erat quis mattis. Integer et viverra elit, vitae
+      condimentum nunc. Fusce rhoncus, risus eu ultricies feugiat, lorem turpis
+      malesuada nisl, vitae facilisis ex purus a sem. Donec cursus finibus elit,
+      id mattis justo tristique eget. Donec finibus leo ac lectus pulvinar
+      vulputate. Sed tempor nunc nec mi lacinia, sed malesuada dui vestibulum.
+      Duis facilisis magna et facilisis posuere. Nulla tincidunt laoreet sapien
+      quis vehicula. Nam nec fermentum metus. Sed varius, ante at elementum
+      condimentum, ipsum sapien faucibus purus, vitae feugiat sem ipsum sed ex.
+      Duis quis augue vel tellus pretium consectetur. Cras fermentum mi turpis,
+      in lobortis enim iaculis ut. Fusce eget eros faucibus, eleifend magna id,
+      pretium justo. Nam luctus ligula a lacus volutpat, eget tempor nulla
+      dignissim.
+    </p>
+    <h2>Imprensa</h2>
+    <p>
+      Fusce a tincidunt tellus. Proin ornare mattis nisi, ut dictum ligula
+      blandit id. Nulla non ultricies velit. Donec ultricies at augue eget
+      hendrerit. Nullam et sem a tortor luctus facilisis quis quis nunc. Mauris
+      eu ligula augue. In id enim magna.
+    </p>
   </div>
 )
 
