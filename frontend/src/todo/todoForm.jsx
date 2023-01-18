e4b9e099e78817bc6c31765e9172a73a5db9df92
Erick Costa <erickcosta2509@gmail.com>
2023-01-18 14:31:46 -0300

    implementação de adicionar e leitura do input (onChange)

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,17 +2,19 @@ import React from "react"
 import Grid from "../template/grid"
 import IconButton from "../template/iconButton"
 
-const TodoForm = () => (
+const TodoForm = (props) => (
   <div role="form" className="todoForm">
     <Grid cols="12 9 10">
       <input
         id="description"
         className="form-control"
         placeholder="Adicione uma tarefa"
+        onChange={props.handleChange}
+        value={props.description}
       />
     </Grid>
     <Grid cols="12 3 2">
-      <IconButton theme="primary" icon="plus" />
+      <IconButton theme="primary" icon="plus" onClick={props.handleAdd} />
     </Grid>
   </div>
 )
