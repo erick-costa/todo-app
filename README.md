8c6367a374eaf05778f41fe970385f033b90e74d
Erick Costa <erickcosta2509@gmail.com>
2023-01-18 13:20:58 -0300

    adicionado informações no readme e estrutura do formulario

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # To do App
+
+Aplicação de gerenciamento de tarefas com React.
+
+## Instalação do projeto
+
+### Back-end
+
+```sh
+npm i --save
+```
+
+### Front-end
+
+```sh
+npm install
+```
+
+### Para rodar o back-end
+
+```sh
+npm run production
+```
+
+### Para rodar o front-end
+
+```sh
+npm start
+```
--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -4,7 +4,7 @@ const Menu = (props) => (
   <nav className="navbar navbar-inverse bg-inverse">
     <div className="container">
       <div className="navbar-header">
-        <a href="#/" className="navbar-brand">
+        <a href="/" className="navbar-brand">
           <i className="fa fa-calendar-check-o"></i> TodoApp
         </a>
       </div>
